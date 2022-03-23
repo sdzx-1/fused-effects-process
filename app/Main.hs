2de39517bcab5537ad26fdca0b633d2c7af0e877
EMQ-YangM <yangm@emqx.io>
2022-03-23 13:08:17 +0800

    remove modeule E1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import E1
+import Example
 
 main :: IO ()
 main = runmProcess
--- src/Example.hs
+++ src/Example.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -13,135 +12,413 @@
 module Example where
 
 import Control.Algebra
+import Control.Carrier.Error.Either
 import Control.Carrier.Reader
 import Control.Carrier.State.Strict
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Exception (SomeException)
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Data (Proxy (Proxy))
--- import Data.Vector (fromList)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Time
 import Process.HasServer
 import Process.HasWorkGroup
 import Process.Metric
-import Process.TH (fromList, mkMetric, mkSigAndClass)
+import Process.TH
 import Process.Type
 import Process.Util
+import Text.Read (readMaybe)
 
------- client - server
-data GetVal where
-  GetVal :: RespVal Int %1 -> GetVal
+-------------------------------------log server
 
-data PrintVal where
-  PrintVal :: Int -> PrintVal
-
-data PutVal where
-  PutVal :: Int -> PutVal
+data Log where
+  Log :: String -> Log
+  Warn :: String -> Log
+  Error :: String -> Log
 
 mkSigAndClass
-  "SigM"
-  [ ''GetVal,
-    ''PrintVal,
-    ''PutVal
+  "SigLog"
+  [ ''Log
   ]
 
---------------- work -- manager
+logServer :: (Has (MessageChan SigLog) sig m, MonadIO m) => m ()
+logServer = forever $ do
+  withMessageChan @SigLog $ \case
+    SigLog1 (Log st) -> liftIO $ putStrLn $ "😀: " ++ st
+    SigLog1 (Warn st) -> liftIO $ putStrLn $ "👿: " ++ st
+    SigLog1 (Error st) -> liftIO $ putStrLn $ "☠️: " ++ st
 
-data A where
-  A :: RespVal Int %1 -> A
+-------------------------------------exception or terminate
 
-data B where
-  B :: Int -> B
+data ProcessR where
+  ProcessR :: Int -> (Either SomeException ()) -> ProcessR
 
-mkSigAndClass
-  "SigW"
-  [ ''A,
-    ''B
-  ]
-
--------------------
+mkSigAndClass "SigException" [''ProcessR]
 
 mkMetric
-  "Smetric"
-  [ "s_total_get",
-    "s_total_put",
-    "s_all"
+  "ETmetric"
+  [ "all_et_exception",
+    "all_et_terminate",
+    "all_et_nothing",
+    "all_et_cycle"
   ]
 
-server ::
-  ( Has
-      ( MessageChan SigW
-          :+: MessageChan SigM
+data EotConfig = EotConfig
+  { einterval :: Int,
+    etMap :: TVar (IntMap (MVar Result))
+  }
+
+eotProcess ::
+  ( HasServer "et" SigException '[ProcessR] sig m,
+    HasServer "log" SigLog '[Log] sig m,
+    Has
+      ( Reader EotConfig
           :+: State Int
-          :+: Metric Smetric
+          :+: Metric ETmetric
       )
       sig
       m,
     MonadIO m
   ) =>
   m ()
-server = forever $ do
-  inc s_all
-  withTwoMessageChan @SigW @SigM
-    ( \case
-        SigW1 (A rsp) -> withResp rsp (pure 1)
-        SigW2 (B i) -> liftIO $ putStrLn $ "cast val is " ++ show i
-    )
-    ( \case
-        SigM1 (GetVal pv) ->
-          withResp
-            pv
-            ( do
-                liftIO (print "server must response")
-                liftIO $ threadDelay 1000
-                inc s_total_get
-                get @Int >>= pure
-            )
-        SigM2 (PrintVal i) -> do
-          all_metrics <- getAll @Smetric Proxy
-          liftIO $ print (all_metrics, i)
-        SigM3 (PutVal val) ->
-          do
-            liftIO (print "put val")
-            inc s_total_put
-            put val
-    )
+eotProcess = forever $ do
+  inc all_et_cycle
+  tvar <- asks etMap
+  tmap <- liftIO $ readTVarIO tvar
+  flip IntMap.traverseWithKey tmap $ \_ tv -> do
+    liftIO (tryTakeMVar tv) >>= \case
+      Nothing -> do
+        inc all_et_nothing
+        pure ()
+      Just (Result tim pid res) -> do
+        case res of
+          Left _ -> inc all_et_exception
+          Right _ -> inc all_et_terminate
+        cast @"et" (ProcessR pid res)
+  interval <- asks einterval
+  allMetrics <- getAll @ETmetric Proxy
+  cast @"log" $ Log (show allMetrics)
+  liftIO $ threadDelay interval
 
-client ::
-  ( (HasWorkGroup "w" SigW '[A, B] sig m),
-    (HasServer "m" SigM '[GetVal, PrintVal, PutVal]) sig m,
-    Has (Reader (TChan (Some SigM))) sig m,
+-------------------------------------process timeout checker
+data TimeoutCheckFinish = TimeoutCheckFinish
+
+data StartTimoutCheck where
+  StartTimoutCheck :: RespVal [(Int, MVar TimeoutCheckFinish)] %1 -> StartTimoutCheck
+
+data ProcessTimeout where
+  ProcessTimeout :: Int -> ProcessTimeout
+
+mkSigAndClass
+  "SigTimeoutCheck"
+  [ ''StartTimoutCheck,
+    ''ProcessTimeout
+  ]
+
+mkMetric
+  "PTmetric"
+  [ "all_pt_cycle",
+    "all_pt_timeout",
+    "all_pt_tcf"
+  ]
+
+newtype PtConfig = PtConfig
+  { ptctimeout :: Int
+  }
+
+ptcProcess ::
+  ( HasServer "log" SigLog '[Log] sig m,
+    HasServer
+      "ptc"
+      SigTimeoutCheck
+      '[StartTimoutCheck, ProcessTimeout]
+      sig
+      m,
+    Has (Reader PtConfig :+: Metric PTmetric) sig m,
     MonadIO m
   ) =>
   m ()
-client = do
-  chan <- ask @(TChan (Some SigM))
-  createWorker @SigW $ \_ c ->
+ptcProcess = forever $ do
+  allMetrics <- getAll @PTmetric Proxy
+  cast @"log" $ Warn $ show allMetrics
+  inc all_pt_cycle
+  res <- call @"ptc" StartTimoutCheck
+  tim <- asks ptctimeout
+  liftIO $ threadDelay tim
+  forM_ res $ \(pid, tmv) -> do
+    liftIO (tryTakeMVar tmv) >>= \case
+      Nothing -> do
+        inc all_pt_timeout
+        cast @"ptc" (ProcessTimeout pid)
+      Just TimeoutCheckFinish -> do
+        inc all_pt_tcf
+        pure ()
+
+-------------------------------------Manager - Work, Manager
+
+data Stop where
+  Stop :: Stop
+
+data Info where
+  Info :: RespVal (Int, String) %1 -> Info
+
+data ProcessStartTimeoutCheck where
+  ProcessStartTimeoutCheck :: RespVal TimeoutCheckFinish %1 -> ProcessStartTimeoutCheck
+
+data ProcessWork where
+  ProcessWork :: IO () -> RespVal () %1 -> ProcessWork
+
+mkSigAndClass
+  "SigCommand"
+  [ ''Stop,
+    ''Info,
+    ''ProcessStartTimeoutCheck,
+    ''ProcessWork
+  ]
+
+data Create where
+  Create :: Create
+
+data GetInfo where
+  GetInfo :: RespVal [(Int, String)] %1 -> GetInfo
+
+data StopProcess where
+  StopProcess :: Int -> StopProcess
+
+data StopAll where
+  StopAll :: StopAll
+
+data KillProcess where
+  KillProcess :: Int -> KillProcess
+
+data Fwork where
+  Fwork :: [IO ()] -> Fwork
+
+mkSigAndClass
+  "SigCreate"
+  [ ''Create,
+    ''GetInfo,
+    ''StopProcess,
+    ''KillProcess,
+    ''Fwork,
+    ''StopAll
+  ]
+
+mProcess ::
+  ( HasServer "log" SigLog '[Log] sig m,
+    Has
+      ( MessageChan SigTimeoutCheck
+          :+: MessageChan SigException
+          :+: MessageChan SigCreate
+          :+: MessageChan SigLog
+      )
+      sig
+      m,
+    HasWorkGroup
+      "w"
+      SigCommand
+      '[ Stop,
+         Info,
+         ProcessStartTimeoutCheck,
+         ProcessWork
+       ]
+      sig
+      m,
+    MonadIO m
+  ) =>
+  m ()
+mProcess = forever $ do
+  withThreeMessageChan
+    @SigTimeoutCheck
+    @SigException
+    @SigCreate
+    ( \case
+        SigTimeoutCheck1 (StartTimoutCheck rsp) ->
+          withResp
+            rsp
+            ( do
+                cast @"log" $ Log "send all check message to works"
+                sendAllCall @"w" ProcessStartTimeoutCheck
+            )
+        SigTimeoutCheck2 (ProcessTimeout pid) -> do
+          cast @"log" $ Error $ "pid: " ++ show pid ++ " health check timeout!!!"
+    )
+    ( \case
+        SigException1 (ProcessR i res) -> do
+          cast @"log" $ Warn $ "some process terminate " ++ show (i, res)
+          clearTVar @SigCommand i -- clean tvar
+          cast @"log" $ Error $ "some tVar clear: [" ++ show i ++ "]"
+          deleteChan @SigCommand i -- remove process channel
+    )
+    ( \case
+        SigCreate1 Create -> do
+          cast @"log" $ Warn "fork a work process"
+          slog <- ask
+          createWorker @SigCommand $ \idx ch ->
+            void $
+              runWorkerWithChan ch $
+                runReader (WorkInfo idx) $
+                  runError @TerminateProcess $
+                    runWithServer @"log"
+                      slog
+                      mWork
+        SigCreate2 (GetInfo rsp) -> withResp rsp (callAll @"w" Info)
+        SigCreate3 (StopProcess i) -> do
+          castById @"w" i Stop
+          deleteChan @SigCommand i
+        SigCreate4 (KillProcess i) -> do
+          killWorker @SigCommand i
+          deleteChan @SigCommand i
+        SigCreate5 (Fwork ios) -> do
+          res <- sendWorks @"w" ios ProcessWork
+          liftIO $ print $ snd res
+        SigCreate6 StopAll -> do
+          castAll @"w" Stop
+    )
+
+-------------------------------------Manager - Work, Work
+
+newtype WorkInfo = WorkInfo
+  { workPid :: Int
+  }
+
+data TerminateProcess = TerminateProcess
+
+mWork ::
+  ( HasServer "log" SigLog '[Log] sig m,
+    Has
+      ( MessageChan SigCommand
+          :+: Reader WorkInfo
+          :+: Error TerminateProcess
+      )
+      sig
+      m,
+    MonadIO m
+  ) =>
+  m ()
+mWork = forever $ do
+  withMessageChan @SigCommand $ \case
+    SigCommand1 Stop -> do
+      pid <- asks workPid
+      cast @"log" $ Warn $ "terminate process: " ++ show pid
+      throwError TerminateProcess
+    SigCommand2 (Info rsp) ->
+      withResp
+        rsp
+        ( do
+            pid <- asks workPid
+            pure (pid, "work is running")
+        )
+    SigCommand3 (ProcessStartTimeoutCheck rsp) ->
+      withResp
+        rsp
+        ( do
+            pid <- asks workPid
+            cast @"log" $ Warn $ "process " ++ show pid ++ " response timoue check"
+            pure TimeoutCheckFinish
+        )
+    SigCommand4 (ProcessWork work rsp) -> do
+      withResp
+        rsp
+        ( do
+            liftIO work
+            pure ()
+        )
+
+------------------------ create client
+client ::
+  ( HasServer "log" SigLog '[Log] sig m,
+    HasServer
+      "s"
+      SigCreate
+      '[ Create,
+         GetInfo,
+         StopProcess,
+         KillProcess,
+         Fwork,
+         StopAll
+       ]
+      sig
+      m,
+    MonadIO m
+  ) =>
+  m ()
+client = forever $ do
+  cast @"log" $ Log "input "
+  val <- liftIO getLine
+  case readMaybe @Int val of
+    Just 0 -> do
+      cast @"s" StopAll
+    Just 5 -> do
+      cast @"s" $ Fwork [print 1, print 2, print 3]
+    Just n -> do
+      cast @"log" $ Log $ "input value is: " ++ show n
+      -- cast @"s" $ StopProcess n
+      cast @"s" $ KillProcess n
+    Nothing -> do
+      cast @"s" Create
+      cast @"log" $ Log "cast create "
+      res <- call @"s" GetInfo
+      cast @"log" $ Log $ "all info: " ++ show res
+
+----------------- run mProcess
+
+runmProcess :: IO ()
+runmProcess = do
+  print "create resource"
+
+  stimeout <- newMessageChan @SigTimeoutCheck
+  se <- newMessageChan @SigException
+  sc <- newMessageChan @SigCreate
+  slog <- newMessageChan @SigLog
+  tvar <- newTVarIO IntMap.empty
+
+  print "fork log server"
+  forkIO $
     void $
-      runWorkerWithChan c $
-        runServerWithChan chan $
-          runState @Int 0 $
-            runMetric @Smetric server
+      runServerWithChan slog logServer
 
-  la <- callAll @"w" A
-  liftIO $ print la
-  castAll @"w" $ B 10
+  print "fork et process"
+  forkIO $
+    void $
+      runWithServer @"et" se $
+        runWithServer @"log" slog $
+          runMetric @ETmetric $
+            runReader (EotConfig 1000000 tvar) $
+              runState @Int
+                1
+                eotProcess
 
-  val <- call @"m" GetVal
-  cast @"m" $ PrintVal val
-  forM_ [0 .. 5] $ \i -> do
-    cast @"m" (PutVal i)
-    timeoutCall @"m" 10000 GetVal >>= \case
-      Nothing -> liftIO $ print "timeout"
-      Just val -> cast @"m" $ PrintVal val
+  print "fork ptc process"
+  forkIO $
+    void $
+      runWithServer @"ptc" stimeout $
+        runMetric @PTmetric $
+          runWithServer @"log" slog $
+            runReader
+              (PtConfig 1000000)
+              ptcProcess
 
-m :: IO ()
-m = do
-  chanM <- newMessageChan @SigM
-  chanW <- newMessageChan @SigW
+  print "fork server process"
+  forkIO $
+    void $
+      runServerWithChan stimeout $
+        runServerWithChan se $
+          runServerWithChan sc $
+            runWithServer @"log" slog $
+              runReader slog $
+                runWithWorkGroup' @"w"
+                  tvar
+                  mProcess
 
-  runWithServer @"m" @SigM chanM $
-    runWithWorkGroup @"w" @SigW $
-      runReader chanM client
+  print "fork client"
+  forkIO $
+    void $
+      runWithServer @"log" slog $
+        runWithServer @"s" sc client
 
-  threadDelay 1000000
+  forever $ do
+    threadDelay 10000000
