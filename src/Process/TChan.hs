1c7b232aaf870e509ea1da68316a5ae34917e432
EMQX-YangM <yangm@emqx.io>
2022-04-01 22:53:26 +0800

    use TQueue replace TChan

--- src/Process/TChan.hs
+++ src/Process/TChan.hs
@@ -8,23 +8,25 @@ module Process.TChan
 where
 
 import qualified Control.Concurrent.STM as T
-import qualified Control.Concurrent.STM.TChan as T
-import qualified Control.Concurrent.STM.TVar as T
-import Data.IORef
+import Control.Concurrent.STM.TQueue
+  ( newTQueue,
+    readTQueue,
+    writeTQueue,
+  )
 
-data TChan a = TChan (T.TVar Int) (T.TChan a)
+data TChan a = TChan (T.TVar Int) (T.TQueue a)
 
 writeTChan :: TChan a -> a -> T.STM ()
 writeTChan (TChan tv tc) a = do
-  T.writeTChan tc a
+  writeTQueue tc a
   T.modifyTVar tv (+ 1)
 
 newTChanIO :: IO (TChan a)
-newTChanIO = T.atomically $ TChan <$> T.newTVar 0 <*> T.newTChan
+newTChanIO = T.atomically $ TChan <$> T.newTVar 0 <*> newTQueue
 
 readTChan :: TChan a -> T.STM a
 readTChan (TChan tv tc) = do
-  v <- T.readTChan tc
+  v <- readTQueue tc
   T.modifyTVar' tv (\x -> x - 1)
   pure v
 
