382842d192961126b56fbe3c929174de974eed62
EMQX-YangM <yangm@emqx.io>
2022-05-23 21:49:54 +0800

    add flushTQueue

--- src/Process/TChan.hs
+++ src/Process/TChan.hs
@@ -3,6 +3,7 @@ module Process.TChan
     writeTChan,
     readTChan,
     newTChanIO,
+    flushTQueue,
     getChanSize,
   )
 where
@@ -19,7 +20,7 @@ data TChan a = TChan (T.TVar Int) (T.TQueue a)
 writeTChan :: TChan a -> a -> T.STM ()
 writeTChan (TChan tv tc) a = do
   writeTQueue tc a
-  T.modifyTVar tv (+ 1)
+  T.modifyTVar' tv (+ 1)
 
 newTChanIO :: IO (TChan a)
 newTChanIO = T.atomically $ TChan <$> T.newTVar 0 <*> newTQueue
@@ -29,6 +30,12 @@ readTChan (TChan tv tc) = do
   v <- readTQueue tc
   T.modifyTVar' tv (\x -> x - 1)
   pure v
+
+flushTQueue :: TChan a -> T.STM [a]
+flushTQueue (TChan tv tc) = do
+  vals <- T.flushTQueue tc
+  T.modifyTVar' tv (const 0)
+  pure vals
 
 getChanSize :: TChan a -> IO Int
 getChanSize (TChan tv _) = T.atomically $ T.readTVar tv
